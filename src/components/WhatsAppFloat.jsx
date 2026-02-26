719b4e89381aa0ab1386b3be141e7f567cbe39ff
krishna pal <kpgangwar8909087801@gmail.com>
2026-02-26 22:24:26 +0530

    update

--- src/components/WhatsAppFloat.jsx
+++ src/components/WhatsAppFloat.jsx
@@ -1,25 +1,3 @@
-// import "../styles/whatsapp.css";
-// import { FaWhatsapp } from "react-icons/fa";
-
-// const WhatsAppFloat = () => {
-// //   const msg = "Hello Krishna Tech 👋 I want a website.";
-// const msg = `Hello Krishna Tech 👋
-// Mujhe website banwani hai
-// Please details share kare`;
-//   return (
-//     <a
-//       href={`https://wa.me/918909087801?text=${encodeURIComponent(msg)}`}
-//       target="_blank"
-//       rel="noreferrer"
-//       className="whatsapp-float"
-//     >
-//       <FaWhatsapp />
-//     </a>
-//   );
-// };
-
-// export default WhatsAppFloat;
-
 
 import "../styles/whatsapp.css";
 import { FaWhatsapp } from "react-icons/fa";
